80a4ab48e14847a9bba23bb0f7dd93ad03d167cc
VictorChavez0501 <andreus4720.6@gmail.com>
2026-07-15 20:21:11 -0600

    chapter 12

--- nextjs-dashboard/app/dashboard/invoices/create/id/edit/page.tsx
+++ nextjs-dashboard/app/dashboard/invoices/create/id/edit/page.tsx
@@ -1,30 +1,27 @@
 import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
-
 import {
   fetchInvoiceById,
   fetchCustomers,
 } from '@/app/lib/data';
+import { notFound } from 'next/navigation';
 
-export default async function Page(
-  props: {
-    params: Promise<{
-      id: string;
-    }>;
-  },
-) {
-  const params =
-    await props.params;
-
+export default async function Page(props: {
+  params: Promise<{
+    id: string;
+  }>;
+}) {
+  const params = await props.params;
   const id = params.id;
 
-  const [
-    invoice,
-    customers,
-  ] = await Promise.all([
+  const [invoice, customers] = await Promise.all([
     fetchInvoiceById(id),
     fetchCustomers(),
   ]);
+
+  if (!invoice) {
+    notFound();
+  }
 
   return (
     <main>
@@ -32,22 +29,17 @@ export default async function Page(
         breadcrumbs={[
           {
             label: 'Invoices',
-            href:
-              '/dashboard/invoices',
+            href: '/dashboard/invoices',
           },
           {
-            label:
-              'Edit Invoice',
+            label: 'Edit Invoice',
             href: `/dashboard/invoices/${id}/edit`,
             active: true,
           },
         ]}
       />
 
-      <Form
-        invoice={invoice}
-        customers={customers}
-      />
+      <Form invoice={invoice} customers={customers} />
     </main>
   );
 }
